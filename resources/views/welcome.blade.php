1c350e7d4653fb4a92e26466eafacd21727eda24
williamfreire <williamff11@gmail.com>
2020-07-05 17:17:03 -0300

    fix bug deployment app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    William
+                    Laravel
                 </div>
 
                 <div class="links">
