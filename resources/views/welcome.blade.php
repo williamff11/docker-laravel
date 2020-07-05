25ce43e375bd7723b3e906f535e8914835e1656d
williamfreire <williamff11@gmail.com>
2020-07-05 13:23:34 -0300

    change view welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    William
                 </div>
 
                 <div class="links">
